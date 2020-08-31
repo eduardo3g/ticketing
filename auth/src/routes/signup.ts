65ac2e840e363e10e9ac4bff6f091440b5d890b8
eduardo3g <eduardosbrasil10@gmail.com>
2020-08-31 20:43:23 -0300

    feat: Handling sign up validation errors.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,28 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Hi there!');
+router.post('/api/users/signup', [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be between 4 and 20 characters')
+  ], (request: Request, response: Response) => {
+  const errors = validationResult(request);
+
+  if (!errors.isEmpty()) {
+    return response.status(400).send(errors.array());
+  }
+
+  const { email, password } = request.body;
+
+  console.log('Creating a user...');
+
+  response.send({});
 });
 
 export { router as signupRouter };
