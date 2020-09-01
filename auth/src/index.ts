dca4a353b9cf100bb2faf86c79508e33095a8272
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-01 14:40:22 -0300

    Connecting to MongoDB 🥬

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -24,6 +25,21 @@ app.all('*', async (request, response) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000 🔥');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log('Connected to MongoDB 🥬');
+  } catch (error) {
+    console.log(error);
+  }
+  app.listen(3000, () => {
+    console.log('Listening on port 3000 🔥');
+  });
+}
+
+start();
