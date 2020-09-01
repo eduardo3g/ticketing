7071cdb1335446b9faff67f6ff44aeeb56325078
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-01 17:46:03 -0300

    chore: Bad request erro handling.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { User } from '../models/User';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequesError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -25,8 +26,7 @@ router.post('/api/users/signup', [
   const existingUser = await User.findOne({ email });
 
   if (existingUser) {
-    console.log('Email in use');
-    return response.send({});
+    throw new BadRequesError('Email in use');
   }
 
   const user = User.build({ email, password });
