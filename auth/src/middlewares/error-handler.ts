ff3fd5b564f860129e52cf10fcf9ccf655616991
eduardo3g <eduardosbrasil10@gmail.com>
2020-08-31 21:52:53 -0300

    chore: Subclassing custom errors.

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -9,6 +9,6 @@ export const errorHandler = (
   console.log('Something went wrong.', error);
 
   response.status(400).send({
-    message: 'Something went wrong',
+    message: error.message,
   });
 };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -15,12 +17,12 @@ router.post('/api/users/signup', [
   const errors = validationResult(request);
 
   if (!errors.isEmpty()) {
-    throw new Error('Invalid email or password');
+    throw new RequestValidationError(errors.array());
   }
 
   console.log('Creating a user...');
 
-  throw new Error('Error connecting to database');
+  throw new DatabaseConnectionError();
 
   response.send({});
 });
