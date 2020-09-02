864d707345d4babae140bc87f69751dad683e2af
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-01 21:56:51 -0300

    chore: Generating JWT.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/User';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequesError } from '../errors/bad-request-error';
@@ -31,6 +33,16 @@ router.post('/api/users/signup', [
 
   const user = User.build({ email, password });
   await user.save();
+
+  const userJwt = jwt.sign({
+    id: user.id,
+    email: user.email,
+  }, 'dj13çljesaud9031ewdnsa');
+
+  // Store JWT on session object
+  request.session = {
+    jwt: userJwt,
+  };
 
   return response.status(201).send(user);
 });
