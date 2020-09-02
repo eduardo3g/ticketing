440057acda3497175f47a42b79c09bc168f1798e
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-02 16:56:25 -0300

    feat: Returning the current user.

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+router.get('/api/users/currentuser',
+  (request: Request,response: Response) => {
+    if (!request.session?.jwt) {
+      return response.send({ currentUser: null })
+    }
+
+    try {
+      const payload = jwt.verify(
+        request.session.jwt,
+        process.env.JWT_KEY!,
+      );
+
+      response.send({ currentUser: payload });
+    } catch (error) {
+      response.send({ currentUser: null });
+    }
 });
 
 export { router as currentUserRouter };
