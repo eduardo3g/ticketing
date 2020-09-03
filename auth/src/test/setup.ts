a9d2d8d169753b90d3aa472944bfbdf57daeb4ff
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-02 21:24:10 -0300

    fix: Added JWT_KEY environment variable to test environment.

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,8 @@ import { app } from '../app';
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = 'somerandomjwttestkey'
+
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
