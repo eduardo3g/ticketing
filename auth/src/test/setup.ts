b4e193464d7c6bec886df546dd4f016c25896e2b
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-02 22:43:48 -0300

    test: Created auth global helper function to return user cookie.

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>
+    }
+  }
+}
 
 let mongo: any;
 
@@ -28,3 +37,28 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email,
+      password,
+    })
+    .expect(200);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
