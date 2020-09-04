d59402a08909d2666457c4bc4042842e8f9aa34d
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-04 20:28:33 -0300

    Finished client service.

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -12,11 +12,12 @@ export default ({ url, method, body, onSuccess }) => {
       if (onSuccess) {
         onSuccess(response.data);
       }
+
       return response.data;
     } catch (err) {
       setErrors(
         <div className="alert alert-danger">
-          <h4>Ooops...</h4>
+          <h4>Ooops....</h4>
           <ul className="my-0">
             {err.response.data.errors.map(err => (
               <li key={err.message}>{err.message}</li>
@@ -27,8 +28,5 @@ export default ({ url, method, body, onSuccess }) => {
     }
   };
 
-  return {
-    doRequest,
-    errors
-  };
+  return { doRequest, errors };
 };
