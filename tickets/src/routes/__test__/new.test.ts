cb5d57b6564c2130af355b1e99924d58244dce25
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-05 14:08:27 -0300

    test: Test file boilerplate.

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('Should have a route handler listening to /api/tickets for post requests', async () => {});
+
+it('Should only be accessed if the user is logged in', async () => {});
+
+it('Should return an error if an invalid title is provided', async () => {});
+
+it('Should return an error if an invalid price is provided', async () => {});
+
+it('Should create a ticket with valid inputs', async () => {});
