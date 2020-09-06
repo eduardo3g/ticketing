4778a1a5e37642f377b743a2e543238ab92bee74
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-05 22:57:00 -0300

    feat: Created tests with JEST and Supertest for tickets routes.

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -9,10 +9,68 @@ it('Should have a route handler listening to /api/tickets for post requests', as
   expect(response.status).not.toEqual(404);
 });
 
-it('Should only be accessed if the user is logged in', async () => {});
+it('Should only be accessed if the user is logged in', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .send({})
+    .expect(401);
+});
 
-it('Should return an error if an invalid title is provided', async () => {});
+it('Should return a status different of 401 if user is authenticated', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({});
 
-it('Should return an error if an invalid price is provided', async () => {});
+  expect(response.status).not.toEqual(401);
+});
 
-it('Should create a ticket with valid inputs', async () => {});
+it('Should return an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10
+    })
+    .expect(400);
+});
+
+it('Should return an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'Valid title',
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'Valid title',
+    })
+    .expect(400);  
+});
+
+it('Should create a ticket with valid inputs', async () => {
+  // Add in a check to make sure a ticket was saved
+
+  await request(app)
+  .post('/api/tickets')
+  .send({
+    title: 'Valid title',
+    price: 20,
+  })
+  .expect(201);
+});
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@e3gtickets/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', (request: Request, response: Response) => {
+router.post('/api/tickets', requireAuth, [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is required'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be greater than 0'), 
+], validateRequest, (request: Request, response: Response) => {
   response.sendStatus(200);
 });
 
