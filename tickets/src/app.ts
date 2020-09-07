598b16d140bed47729e5dcf13da536df5d5368b6
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-07 00:15:48 -0300

    Complete Index route implementation.

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -11,6 +11,7 @@ import {
 
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
 
 
 const app = express();
@@ -27,6 +28,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (request, response) => {
   throw new NotFoundError();
