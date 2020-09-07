3fd19c9254081a3cb171e6cdc929947543ff9820
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-07 11:38:01 -0300

    test: Checking permission on ticket update.

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -28,7 +28,22 @@ it('Should return 401 if the user is not authenticated', async () => {
 });
 
 it('Should return 401 if the user does not own the ticket', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'Valid title',
+      price: 20
+    });
 
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', global.signin()) // generates a new user id
+    .send({
+      title: 'New valid title',
+      price: 1000
+    })
+    .expect(401);
 });
 
 it('Should return 400 if the user provides an invalid title or price', async () => {
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -10,14 +10,18 @@ import { Ticket } from '../models/Ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (
-  request: Request,
-  response: Response
-) => {
+router.put(
+  '/api/tickets/:id',
+  requireAuth,
+  async (request: Request, response: Response) => {
   const ticket = await Ticket.findById(request.params.id);
 
   if (!ticket) {
     throw new NotFoundError();
+  }
+
+  if (ticket.userId !== request.currentUser!.id) {
+    throw new NotAuthorizedError();
   }
 
   response.send(ticket);
