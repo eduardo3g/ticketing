46d1e9a63e511172c7fa9d0c4a82ebf162426706
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-06 23:50:00 -0300

    feat: Adding valid ObjectID to ticket object.

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest';
+import mongoose from 'mongoose';
 import { app } from '../../app';
 
 it('Should return a 404 if the ticket is not found', async () => {
+  const id  = new mongoose.Types.ObjectId().toHexString();
+
   await request(app)
-    .get('/api/tickets/nonexistingticket')
+    .get(`/api/tickets/${id}`)
     .send()
     .expect(404);
 });
