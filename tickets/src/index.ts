4a7efbc70b6a98cc8787f6d284be80a9aa11f132
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-12 14:31:53 -0300

    feat: create NATS streaming server singleton

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
-
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -12,6 +12,8 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', 'jaklj1k3lsa', 'http://nats-srv:4222');
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@e3gtickets/common';
 import { Ticket } from '../models/Ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -23,6 +25,13 @@ router.post('/api/tickets', requireAuth, [
   });
 
   await ticket.save();
+
+  await new TicketCreatedPublisher(natsWrapper.client).publish({
+    id: ticket.id,
+    title: ticket.title,
+    price: ticket.price,
+    userId: ticket.userId,
+  });
 
   response.status(201).send(ticket);
 });
