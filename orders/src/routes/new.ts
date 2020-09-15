172e89b6c432ce8049f8133629f1cf3c0ec001f0
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-14 21:14:27 -0300

    chore: initial new order router structure

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,11 +1,22 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@e3gtickets/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (
-  request: Request, response: Response
-) => {
-  response.send({});
+router.post('/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided')
+  ],
+  validateRequest,
+  async (request: Request, response: Response) => {
+    response.send({});
 });
 
 export { router as newOrderRouter };
