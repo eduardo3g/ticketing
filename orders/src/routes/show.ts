a6510bca7130d7fd8b2a8cbaff4cc175297cde2e
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-21 19:40:48 -0300

    feat: fetch individual orders

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@e3gtickets/common';
+import { Order } from '../models/Order';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (
+router.get('/api/orders/:orderId', requireAuth, async (
   request: Request, response: Response
 ) => {
-  response.send({});
+  const order = await Order.findById(request.params.orderId).populate('ticket');
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== request.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  response.send(order);
 });
 
 export { router as showOrderRouter };
