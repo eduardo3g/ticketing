d1c0a4e1719bd92cfd766bac072861abe6645cd7
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-22 19:17:12 -0300

    feat: cancel orders

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,11 +1,29 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@e3gtickets/common';
+import { Order, OrderStatus } from '../models/Order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (
+router.delete('/api/orders/:orderId', requireAuth, async (
   request: Request, response: Response
 ) => {
-  response.send({});
+  const { orderId } = request.params;
+
+  const order = await Order.findById(orderId);
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== request.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  order.status = OrderStatus.Cancelled;
+
+  await order.save();
+
+  response.status(204).send({});
 });
 
 export { router as deleteOrderRouter };
