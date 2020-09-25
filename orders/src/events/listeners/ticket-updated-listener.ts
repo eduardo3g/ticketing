583c7f3cd384235aaa0fcadc8bf7f018f970d134
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-24 21:59:50 -0300

    feat: create ticket updated listener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,32 @@
+import { Message } from 'node-nats-streaming';
+import { 
+  Subjects,
+  Listener,
+  TicketUpdatedEvent,
+} from '@e3gtickets/common';
+import { Ticket } from '../../models/Ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  readonly subject = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage (data: TicketUpdatedEvent['data'], message: Message) {
+    const { id, title, price } = data;
+
+    const ticket = await Ticket.findById(id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }    
+
+    ticket.set({
+      title,
+      price,
+    });
+
+    await ticket.save();
+
+    message.ack();
+  }
+}
