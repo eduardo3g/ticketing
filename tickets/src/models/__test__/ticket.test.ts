df31b88a76d79f4a8e0c0e89825f8b2b61df697a
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-25 17:20:36 -0300

    test: optimistic concurrency control

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,5 +1,27 @@
 import { Ticket } from '../Ticket';
 
-it ('Should implement optimistic concurrency control', async () => {
+it ('Should implement optimistic concurrency control', async (done) => {
+  const ticket = await Ticket.build({
+    title: 'Concert',
+    price: 20,
+    userId: '123',
+  });
 
+  await ticket.save();
+
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  firstInstance?.set({ price: 10 });
+  secondInstance?.set({ price: 10 });
+
+  await firstInstance?.save();
+
+  try {
+    await secondInstance?.save();
+  } catch (err) {
+    return done();
+  }
+
+  throw new Error('Should not reach this point');
 });
