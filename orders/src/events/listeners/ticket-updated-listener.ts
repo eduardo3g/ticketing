3f51b4944d6abf58105d9d2ad9a172b7659adcdb
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-25 19:23:48 -0300

    refactor: abstract ticket filter by ID and version inside model

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -12,12 +12,9 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage (data: TicketUpdatedEvent['data'], message: Message) {
-    const { id, title, price, version } = data;
+    const { id, title, price } = data;
 
-    const ticket = await Ticket.findOne({
-      _id: data.id,
-      version: data.version - 1
-    });
+    const ticket = await Ticket.findByEvent(data);
 
     if (!ticket) {
       throw new Error('Ticket not found');
