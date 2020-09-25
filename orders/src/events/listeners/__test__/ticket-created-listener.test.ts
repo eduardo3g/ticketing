b430b6f97ca67f16d56637439e308bed5306fa4f
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-25 20:12:20 -0300

    feat: add order version at order model

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,44 @@
+import { Message } from 'node-nats-streaming';
+import mongoose from 'mongoose';
+import { TicketCreatedEvent } from '@e3gtickets/common';
+import { TicketCreatedListener } from '../ticket-created-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/Ticket';
+
+const setup = async () => {
+  // Create instance of the listener
+  const listener = new TicketCreatedListener(natsWrapper.client);
+
+  // Create fake data event
+  const data: TicketCreatedEvent['data'] = {
+    version: 0,
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'Concert',
+    price: 10,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+
+  // Create a fake message object
+  // @ts-ignore
+  const message: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, message };
+};
+
+it('Should create and save a ticket', async () => {
+  const { listener, data, message } = await setup();
+
+  await listener.onMessage(data, message);
+
+  const ticket = await Ticket.findById(data.id);
+
+  expect(ticket).toBeDefined();
+  expect(ticket!.title).toEqual(data.title);
+  expect(ticket!.price).toEqual(data.price);
+});
+
+it('Should ack the message', async () => {
+
+});
