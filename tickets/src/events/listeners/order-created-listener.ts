8e2d6cd7b561e625964a77a499d28f91de0bb765
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-26 12:28:33 -0300

    feat: reserve a ticket setting an orderId to the document

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,28 @@
+import { Message } from 'node-nats-streaming';
+import {
+  Listener,
+  OrderCreatedEvent,
+  Subjects,
+  NotFoundError
+} from '@e3gtickets/common';
+import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/Ticket';
+
+export class OrderCreatedLisneter extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage (data: OrderCreatedEvent['data'], message: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    ticket.set({ orderId: data.id });
+
+    await ticket.save();
+
+    message.ack();
+  }
+};
