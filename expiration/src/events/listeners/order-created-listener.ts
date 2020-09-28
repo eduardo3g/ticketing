5dd556e7d98a879967fd2d06bb2b249064473003
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-28 19:44:31 -0300

    feat: create base of order created listener

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,14 @@
+import {
+  Listener,
+  Subjects,
+  OrderCreatedEvent
+} from '@e3gtickets/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], message: Message) {}
+}
