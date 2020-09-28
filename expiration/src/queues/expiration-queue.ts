6544ec8400c6568b779591a91c8531a10fdc8a22
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-28 20:00:16 -0300

    chore: initial bull queue setup

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,20 @@
+import Queue from 'bull';
+
+interface Payload {
+  orderId: string;
+};
+
+const expirationQueue = new Queue<Payload>('order:expiration', {
+  redis: {
+    host: process.env.REDIS_HOST,
+  }
+});
+
+expirationQueue.process(async (job) => {
+  console.log(
+    'Publish an expiration:comlpete event for orderId',
+    job.data.orderId
+  );
+});
+
+export { expirationQueue };
