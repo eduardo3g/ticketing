e7865fb4663f21f0fd15524dd969f23ae8bda3dc
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-27 21:12:01 -0300

    feat: publish ticket updated event when a order is created

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,11 +2,11 @@ import { Message } from 'node-nats-streaming';
 import {
   Listener,
   OrderCreatedEvent,
-  Subjects,
-  NotFoundError
+  Subjects
 } from '@e3gtickets/common';
 import { queueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/Ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -22,6 +22,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     ticket.set({ orderId: data.id });
 
     await ticket.save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
 
     message.ack();
   }
