bfb4e812f87a8705f13ef6ba29c45831ee83d8b9
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-28 23:21:53 -0300

    feat: publish expiration complete event on job processing by bull queue

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,8 @@
 import Queue from 'bull';
+import {
+  ExpirationCompletePublisher
+} from '../events/publishers/expiration-complete-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 interface Payload {
   orderId: string;
@@ -11,10 +15,9 @@ const expirationQueue = new Queue<Payload>('order:expiration', {
 });
 
 expirationQueue.process(async (job) => {
-  console.log(
-    'Publish an expiration:complete event for orderId',
-    job.data.orderId
-  );
+  new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId: job.data.orderId,
+  });
 });
 
 export { expirationQueue };
