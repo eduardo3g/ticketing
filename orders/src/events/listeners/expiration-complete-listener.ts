cd8ce4c602b720ad38aa2eb66bb14f72bd2ee55f
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-29 10:09:20 -0300

    feat: handle expiration complete events

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,27 @@
+import {
+  Listener,
+  Subjects,
+  ExpirationCompleteEvent,
+  OrderStatus
+} from '@e3gtickets/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/Order';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  readonly subject = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], message: Message) {
+    const order = await Order.findById(data.orderId);
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+      ticket: null,
+    });
+  }
+}
