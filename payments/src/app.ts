9592f96f9336cd8c3530f25c5c2e8753919df6c4
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-29 20:35:46 -0300

    chore: initial structure

---
+++ payments/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+
+import cookieSession from 'cookie-session';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@e3gtickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.all('*', async (request, response) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
