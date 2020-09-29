f4485d478d33373c1562486006098f79137c9af9
eduardo3g <eduardosbrasil10@gmail.com>
2020-09-29 10:31:41 -0300

    feat: emit order cancelled event

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -7,13 +7,15 @@ import {
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { Order } from '../../models/Order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   readonly subject = Subjects.ExpirationComplete;
   queueGroupName = queueGroupName;
 
   async onMessage(data: ExpirationCompleteEvent['data'], message: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId)
+      .populate('ticket');
 
     if (!order) {
       throw new Error('Order not found');
@@ -22,5 +24,17 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     order.set({
       status: OrderStatus.Cancelled,
     });
+
+    await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    message.ack();
   }
 }
