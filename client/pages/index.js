ad20f823d1581939d1ae7a17625720a7b943d2ff
eduardo3g <eduardosbrasil10@gmail.com>
2020-10-08 19:54:42 -0300

    chore: add client and current user to getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,7 +6,7 @@ const LandingPage = ({ currentUser }) => {
   );
 };
 
-LandingPage.getInitialProps = async context => {
+LandingPage.getInitialProps = async (context, client, currentUser) => {
   return {};
 };
 
