03e4375b27398dd1895062db70ea8ad59e0e3109
eduardo3g <eduardosbrasil10@gmail.com>
2020-10-08 23:09:04 -0300

    feat: create order payment

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,18 @@
 import { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
 
 const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => console.log(payment),
+  });
+
   useEffect(() => {
     const findTimeLeft = () => {
       const msLeft = new Date(order.expiresAt) - new Date();
@@ -25,12 +35,13 @@ const OrderShow = ({ order, currentUser }) => {
     <div>
       Time left to pay: {timeLeft} seconds
       <StripeCheckout
-        token={(token) => console.log(token)}
+        token={({ id }) => doRequest({ token: id })}
         stripeKey={process.env.NEXT_PUBLIC_STRIPE_PUB_KEY}
         amount={order.ticket.price * 100}
         currency="BRL"      
         email={currentUser.email}
       />
+      {errors}
     </div>
   );
 };
