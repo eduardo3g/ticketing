18c709f237f573266993e60b7acbbf2faf34cd1a
eduardo3g <eduardosbrasil10@gmail.com>
2020-10-08 21:47:12 -0300

    feat: add order expiration countdown

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,9 +1,34 @@
-const OrderShow = () => {
+import { useEffect, useState } from 'react';
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState('');
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft( Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    }
+  }, [order]);
+
   return (
     <div>
-      <h1>Order Show</h1>
+      Time left to pay: {timeLeft} seconds
     </div>
   );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
 };
 
 export default OrderShow;
