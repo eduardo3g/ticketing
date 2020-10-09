2559ec1ca242bf6b573832d2acf258f19bc081ae
eduardo3g <eduardosbrasil10@gmail.com>
2020-10-08 23:12:05 -0300

    feat: list tickets that are not attached to any orders

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -4,7 +4,9 @@ import { Ticket } from '../models/Ticket';
 const router = express.Router();
 
 router.get('/api/tickets', async (request: Request, response: Response) => {
-  const tickets = await Ticket.find({});
+  const tickets = await Ticket.find({
+    orderId: undefined,
+  });
 
   response.send(tickets);
 });
